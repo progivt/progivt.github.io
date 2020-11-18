76a4a51c48f0205d895822247f0ab673896bb260
Александр Павлов <7911562+av-pavlov@users.noreply.github.com>
2020-11-19 00:10:24 +0900

    premerge

--- src/lab.js
+++ src/lab.js
@@ -1,1 +1,1 @@
-export var activelab = { "name":"ЛР4. Циклы","deadline":"2020-11-18 23:59","url":"будет позже"};
+export var activelab = { "name":"ЛР4. Циклы","deadline":"2020-11-18 23:59","url":"https://git.io/JkWqP"};
